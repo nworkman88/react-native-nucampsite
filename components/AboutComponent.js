5aef8779385c31058c4be1464c9c7fcc3f439f49
Nick Workman <nicholaszworkman@gmail.com>
2021-10-02 11:25:46 -0400

    10-2 updates 2

---
+++ components/AboutComponent.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class About extends Component {
+    constructor(props)
+    static navigationOptions = {
+        title: 'About Us'
+    }
+    render() {
+        return (
+            <ScrollView>
+                <Card>
+                    <Text style={{ margin: 10 }}>
+                        We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                    </Text>
+                </Card>
+            </ScrollView>
+        )
+    }
+}
+
+export default About;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,46 +1,121 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import CampsiteInfo from "./CampsiteInfoComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 import Constants from 'expo-constants';
-import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
-
+import { View, Platform } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import { createDrawerNavigator } from "react-navigation-drawer";
+import {createAppContainer} from 'react-navigation';
+​
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: { screen: Directory },
-        CampsiteInfo: { screen: CampsiteInfo }
+        Directory: {screen: Directory},
+        CampsiteInfo: {screen: CampsiteInfo}
     },
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
             headerStyle: {
-                backgroundColor: '#5637DD'
+                backgroundColor: '#5637DD',
+                marginTop: Platform.OS === 'ios' ? 25 : 0
+                
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
             }
+            
+        }
+    }
+);
+​
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD',
+                marginTop: Platform.OS === 'ios' ? 25 : 0   
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }    
         }
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD',
+                marginTop: Platform.OS === 'ios' ? 25 : 0   
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }    
+        }
+    }
+);
 
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD',
+                marginTop: Platform.OS === 'ios' ? 25 : 0   
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }    
+        }
+    }
+);
+​
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {screen : HomeNavigator},
+        Directory: {screen : DirectoryNavigator},
+        About: {screen : AboutNavigator},
+        Contact: {screen : ContactNavigator}
+    },
+    {
+        drawerStyle: {
+            backgroundColor: '#CEC8FF',
+            marginTop: Platform.OS === 'ios' ? 100 : 0
+        }
+    }
+);
+​
+const AppNavigator = createAppContainer(MainNavigator);
+​
 class Main extends Component {
-
+​
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
-                }}>
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight,
+                }}
+            >
                 <AppNavigator />
-            </View>
-        );
+            </View> 
+        ); 
     }
 }
-
+​
 export default Main;
